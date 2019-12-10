a5a09eb46d532a2a6873faae4728196442721ff4
zhangyanyu0722 <55149684+zhangyanyu0722@users.noreply.github.com>
2019-12-09 21:16:17 -0500

    final

---
+++ code/MRCNN/object_detect.py
@@ -0,0 +1,156 @@
+# detect kangaroos in photos with mask rcnn model
+from os import listdir
+from xml.etree import ElementTree
+from numpy import zeros
+from numpy import asarray
+from numpy import expand_dims
+from matplotlib import pyplot
+from matplotlib.patches import Rectangle
+from mrcnn.config import Config
+from mrcnn.model import MaskRCNN
+from mrcnn.model import mold_image
+from mrcnn.utils import Dataset
+
+# class that defines and loads the kangaroo dataset
+class KangarooDataset(Dataset):
+	# load the dataset definitions
+	def load_dataset(self, dataset_dir, is_train=True):
+		# define one class
+		self.add_class("dataset", 1, "pole")
+		# define data locations
+		images_dir = dataset_dir + '/images/'
+		annotations_dir = dataset_dir + '/annots/'
+		# find all images
+		for filename in listdir(images_dir):
+			# extract image id
+			image_id = filename[:-5]
+			# skip bad images
+			if image_id in ['00090']:
+				continue
+			# skip all images after 150 if we are building the train set
+			if is_train and int(image_id) >= 11:
+				continue
+			# skip all images before 150 if we are building the test/val set
+			if not is_train and int(image_id) < 11:
+				continue
+			img_path = images_dir + filename
+			ann_path = annotations_dir + image_id + '.xml'
+			# add to dataset
+			self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
+
+	# load all bounding boxes for an image
+	def extract_boxes(self, filename):
+		# load and parse the file
+		root = ElementTree.parse(filename)
+		boxes = list()
+		# extract each bounding box
+		for box in root.findall('.//bndbox'):
+			xmin = int(box.find('xmin').text)
+			ymin = int(box.find('ymin').text)
+			xmax = int(box.find('xmax').text)
+			ymax = int(box.find('ymax').text)
+			coors = [xmin, ymin, xmax, ymax]
+			boxes.append(coors)
+		# extract image dimensions
+		width = int(root.find('.//size/width').text)
+		height = int(root.find('.//size/height').text)
+		return boxes, width, height
+
+	# load the masks for an image
+	def load_mask(self, image_id):
+		# get details of image
+		info = self.image_info[image_id]
+		# define box file location
+		path = info['annotation']
+		# load XML
+		boxes, w, h = self.extract_boxes(path)
+		# create one array for all masks, each on a different channel
+		masks = zeros([h, w, len(boxes)], dtype='uint8')
+		# create masks
+		class_ids = list()
+		for i in range(len(boxes)):
+			box = boxes[i]
+			row_s, row_e = box[1], box[3]
+			col_s, col_e = box[0], box[2]
+			masks[row_s:row_e, col_s:col_e, i] = 1
+			class_ids.append(self.class_names.index('pole'))
+		return masks, asarray(class_ids, dtype='int32')
+
+	# load an image reference
+	def image_reference(self, image_id):
+		info = self.image_info[image_id]
+		return info['path']
+
+# define the prediction configuration
+class PredictionConfig(Config):
+	# define the name of the configuration
+	NAME = "pole_cfg"
+	# number of classes (background + kangaroo)
+	NUM_CLASSES = 1 + 1
+	# simplify GPU config
+	GPU_COUNT = 1
+	IMAGES_PER_GPU = 1
+
+# plot a number of photos with ground truth and predictions
+def plot_actual_vs_predicted(dataset, model, cfg, n_images=4):
+	# load image and mask
+	for i in range(n_images):
+		# load the image and mask
+		image = dataset.load_image(i)
+		mask, _ = dataset.load_mask(i)
+		# convert pixel values (e.g. center)
+		scaled_image = mold_image(image, cfg)
+		# convert image into one sample
+		sample = expand_dims(scaled_image, 0)
+		# make prediction
+		yhat = model.detect(sample, verbose=0)[0]
+		# define subplot
+		pyplot.subplot(n_images, 2, i*2+1)
+		# plot raw pixel data
+		pyplot.imshow(image)
+		pyplot.title('Actual')
+		# plot masks
+		for j in range(mask.shape[2]):
+			pyplot.imshow(mask[:, :, j], cmap='gray', alpha=0.3)
+		# get the context for drawing boxes
+		pyplot.subplot(n_images, 2, i*2+2)
+		# plot raw pixel data
+		pyplot.imshow(image)
+		pyplot.title('Predicted')
+		ax = pyplot.gca()
+		# plot each box
+		for box in yhat['rois']:
+			# get coordinates
+			y1, x1, y2, x2 = box
+			# calculate width and height of the box
+			width, height = x2 - x1, y2 - y1
+			# create the shape
+			rect = Rectangle((x1, y1), width, height, fill=False, color='red')
+			# draw the box
+			ax.add_patch(rect)
+	# show the figure
+	pyplot.show()
+
+# load the train dataset
+train_set = KangarooDataset()
+train_set.load_dataset('pole_test', is_train=True)
+train_set.prepare()
+print('Train: %d' % len(train_set.image_ids))
+# load the test dataset
+test_set = KangarooDataset()
+test_set.load_dataset('pole_test', is_train=False)
+test_set.prepare()
+print('Test: %d' % len(test_set.image_ids))
+# create config
+cfg = PredictionConfig()
+# define the model
+model = MaskRCNN(mode='inference', model_dir='./', config=cfg)
+# load model weights
+model_path = 'mask_rcnn_pole_cfg_0016.h5'
+model.load_weights(model_path, by_name=True)
+# plot predictions for train dataset
+# plot_actual_vs_predicted(train_set, model, cfg)
+# plot predictions for test dataset
+plot_actual_vs_predicted(test_set, model, cfg)
+
+
---
+++ code/MRCNN/train.py
@@ -0,0 +1,116 @@
+# split into train and test set
+from os import listdir
+from xml.etree import ElementTree
+from numpy import zeros
+from numpy import asarray
+from numpy import expand_dims
+from numpy import mean
+from mrcnn.config import Config
+from mrcnn.model import MaskRCNN
+from mrcnn.utils import Dataset
+from mrcnn.utils import compute_ap
+from mrcnn.model import load_image_gt
+from mrcnn.model import mold_image
+
+# class that defines and loads the kangaroo dataset
+class PoleDataset(Dataset):
+	# load the dataset definitions
+	def load_dataset(self, dataset_dir, is_train=True):
+		# define one class
+		self.add_class("dataset", 1, "pole")
+		# define data locations
+		images_dir = dataset_dir + '/images/'
+		annotations_dir = dataset_dir + '/annotations/'
+		# find all images
+		for filename in listdir(images_dir):
+			# extract image id
+			image_id = filename[:-5]
+			# skip bad images
+			# if image_id in ['0089','0094','0080','0057','0014']:
+			# 	continue
+			# skip all images after 150 if we are building the train set
+			if is_train and int(image_id) >= 80:
+				continue
+			# skip all images before 150 if we are building the test/val set
+			if not is_train and int(image_id) < 80:
+				continue
+			img_path = images_dir + filename
+			ann_path = annotations_dir + image_id + '.xml'
+			# add to dataset
+			self.add_image('dataset', image_id=image_id, path=img_path, annotation=ann_path)
+
+	# extract bounding boxes from an annotation file
+	def extract_boxes(self, filename):
+		# load and parse the file
+		tree = ElementTree.parse(filename)
+		# get the root of the document
+		root = tree.getroot()
+		# extract each bounding box
+		boxes = list()
+		for box in root.findall('.//bndbox'):
+			xmin = int(box.find('xmin').text)
+			ymin = int(box.find('ymin').text)
+			xmax = int(box.find('xmax').text)
+			ymax = int(box.find('ymax').text)
+			coors = [xmin, ymin, xmax, ymax]
+			boxes.append(coors)
+		# extract image dimensions
+		width = int(root.find('.//size/width').text)
+		height = int(root.find('.//size/height').text)
+		return boxes, width, height
+
+	# load the masks for an image
+	def load_mask(self, image_id):
+		# get details of image
+		info = self.image_info[image_id]
+		# define box file location
+		path = info['annotation']
+		# load XML
+		boxes, w, h = self.extract_boxes(path)
+		# create one array for all masks, each on a different channel
+		masks = zeros([h, w, len(boxes)], dtype='uint8')
+		# create masks
+		class_ids = list()
+		for i in range(len(boxes)):
+			box = boxes[i]
+			row_s, row_e = box[1], box[3]
+			col_s, col_e = box[0], box[2]
+			masks[row_s:row_e, col_s:col_e, i] = 1
+			class_ids.append(self.class_names.index('pole'))
+		return masks, asarray(class_ids, dtype='int32')
+
+	# load an image reference
+	def image_reference(self, image_id):
+		info = self.image_info[image_id]
+		return info['path']
+
+# define a configuration for the model
+class PoleConfig(Config):
+	# define the name of the configuration
+	NAME = "pole_cfg"
+	# number of classes (background + kangaroo)
+	NUM_CLASSES = 1 + 1
+	# number of training steps per epoch
+	STEPS_PER_EPOCH = 50
+
+# train set
+train_set = PoleDataset()
+train_set.load_dataset('pole', is_train=True)
+train_set.prepare()
+print('Train: %d' % len(train_set.image_ids))
+
+# test/val set
+test_set = PoleDataset()
+test_set.load_dataset('pole', is_train=False)
+test_set.prepare()
+print('Test: %d' % len(test_set.image_ids))
+
+# prepare config
+config = PoleConfig()
+config.display()
+# define the model
+model = MaskRCNN(mode='training', model_dir='./', config=config)
+# load weights (mscoco) and exclude the output layers
+model.load_weights('mask_rcnn_coco.h5', by_name=True, exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",  "mrcnn_bbox", "mrcnn_mask"])
+# train weights (output layers or 'heads')
+model.train(train_set, test_set, learning_rate=config.LEARNING_RATE, epochs=25, layers='heads')
